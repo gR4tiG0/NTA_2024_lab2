453bc34e1ecf928204c8015955b3714d295144d9
gR4tiG0 <yurpry-ipt25@lll.kpi.ua>
2024-05-10 23:05:38 +0300

    oracle comm done

--- sources/test.py
+++ sources/test.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
-from ctypes import CDLL, c_uint64, POINTER
+from ctypes import CDLL, c_uint64
+from colorama import Fore, Style
+from pwn import *
 import logging
-from factorization.factorization import factor
-from sage.all import factor as sage_factor
 import time
-logging.basicConfig(level=logging.DEBUG)
 
 
 LIB_NAME = "./libdlp.so"
 
+
+
+
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and custom prefixes"""
+
+    FORMAT = {
+        logging.DEBUG: Fore.WHITE + "[*] %(asctime)s - %(message)s" + Style.RESET_ALL,
+        logging.INFO: Fore.BLUE + "[+] %(asctime)s - %(message)s" + Style.RESET_ALL,
+        logging.WARNING: Fore.YELLOW + "[!] %(asctime)s - %(message)s" + Style.RESET_ALL,
+        logging.ERROR: Fore.RED + "[!] %(asctime)s - %(message)s" + Style.RESET_ALL,
+        logging.CRITICAL: Fore.RED + "[!] %(asctime)s - %(message)s" + Style.RESET_ALL
+    }
+
+    DATEFMT = '%Y-%m-%d %H:%M:%S'
+
+    def format(self, record):
+        log_fmt = self.FORMAT.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt=self.DATEFMT)
+        return formatter.format(record)
+
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(CustomFormatter())
+logger.addHandler(console_handler)
+logger.setLevel(logging.DEBUG)
+
+logging.getLogger('pwnlib').setLevel(logging.CRITICAL)
+
+
 def initLib() -> CDLL:
-    logging.debug(f"Initializing lib {LIB_NAME}")
+    logger.debug(f"Initializing lib {LIB_NAME}")
     lib = CDLL(LIB_NAME)
     lib.dlp_bruteforce.argtypes = [c_uint64, c_uint64, c_uint64]
     lib.dlp_bruteforce.restype = c_uint64
@@ -36,51 +65,83 @@ def initLib() -> CDLL:
 
     return lib
 
+class DockerOracle:
+    def __init__(self, lib) -> None:
+        self.lib = lib
+        self.command = ['docker', 'run', '-i', 'salo1d/nta_cp2_helper:2.0']
+        self.ok_word = "BINGO" 
+    
+    def startCommunication(self, dec_len:int, second_step:bool = True) -> None:
+        logger.info(f"Starting communication with oracle with dec_len = {dec_len}")
+        r = process(self.command)
+        r.recvuntil(b': ')
+        r.sendline(str(dec_len).encode())
+        inp = r.recvuntil(b'value: ')
+        a,b,p = self.oracleParseInput(inp)
+        logger.debug(f"Received a = {a}, b = {b}, p = {p}")
+        logger.debug(f"Starting SPH")
+
+        start = time.time()
+        res = self.lib.dlp_spg(a,b,p)
+        logger.info(f"Time taken: {time.time()-start}")
+        logger.debug(f"Result {res}")
+        assert pow(a, res, p) == b
+        logger.info(f"SPH passed, sending result to oracle")
+
+        r.sendline(str(res).encode())
+        inp = r.recvuntil(b'value: ')
+        if self.ok_word not in inp.decode():
+            logger.error(f"Oracle failed to validate SPH result")
+            exit(1)
+        
+        logger.debug(f"Oracle validated SPH result")
+
+        if second_step:
+            logger.info(f"Starting Task 2")
+            a,b,p = self.oracleParseInput(inp)
+            logger.debug(f"Received a = {a}, b = {b}, p = {p}")
+            logger.debug(f"Starting SPH")
+
+            start = time.time()
+            res = self.lib.dlp_spg(a,b,p)
+            logger.info(f"Time taken: {time.time()-start}")
+            logger.debug(f"Result {res}")
+            assert pow(a, res, p) == b
+            logger.info(f"SPH passed, sending result to oracle")
+
+            r.sendline(str(res).encode())
+            inp = r.recvuntil(b'closed')
+            if self.ok_word not in inp.decode():
+                logger.error(f"Oracle failed to validate SPH result")
+                exit(1)
+            
+            logger.debug(f"Oracle validated SPH result")
+
+        r.close()
+
+    def oracleParseInput(self, inp: bytes) -> tuple:
+        inp = inp.decode().strip().split(";")
+        a = int(inp[0][1:].split("= ")[-1])
+        b = int(inp[1][1:].split("= ")[-1])
+        p = int(inp[2][1:].split("= ")[-1].split(".")[0])
+        
+        return a,b,p
 
 def main() -> None:
     lib = initLib()
 
-    a,p,b = 25472862553,48600450361,41279512019
-    # start = time.time()
-    # logging.debug(pow(a,-1,p))
-    # logging.debug(time.time()-start)
-    lib.factorizeAndPrint(p-1)
-
-
-    start = time.time()
-    res = lib.dlp_spg(a,b,p)
-
-    logging.debug(time.time()-start)
-    logging.debug(res)
-
-    assert pow(a, res, p) == b
-
-    # # logging.debug("Starting bruteforce approach")
-    # # result = lib.dlp_bruteforce(a, p, b)
-    # # logging.debug(f"Result: {result}")
-
-    # # assert lib.power(a, result, p) == b
-
-    # # factors, powers = factor(p-1)
-    # factors_ = sage_factor(p-1)
-    # factors = [f[0] for f in factors_]
-    # powers = [f[1] for f in factors_]
-    # logging.debug(f"Factors: {factors}")
-    # factors_ = (c_uint64 * len(factors))(*factors)
-    # powers_ = (c_uint64 * len(powers))(*powers)
-
-    # logging.debug(f"Starting spg approach, parameters:")
-    # logging.debug(f"{a = }; {b = }; {p = };")
-    # result = lib.dlp_spg(a, p, b, factors_, powers_, len(factors))
-
-    # logging.debug(f"Result: {result}")
-    # print("----------------")
-    # result = SPG(a, b, p)
-    # logging.debug(f"Result: {result}")
-
-    # assert pow(a, result, p) == b   
-
-
+    oracle = DockerOracle(lib)
+    
+    for i in range(10, 21):
+        try:
+            if i > 12:
+                oracle.startCommunication(i, False)
+            else:
+                oracle.startCommunication(i)
+        except Exception as e:
+            logger.error(f"Exception: {e}")
+            continue
+    
 
 if __name__ == "__main__":
     main()
